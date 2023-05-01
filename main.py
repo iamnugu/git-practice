b82a694cf546fb67a1e02a3514ca9fdb97c8c81d
iamnugu <iamnugu22@gmail.com>
2023-05-01 11:30:26 +0900

    feat : Add items on lists

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-#Test python env
+# Test python env
 def print_hello():
-    animals = ['dog','cat','hamster']
-    foods = ['치킨','햄버거']
-    names = ['John','Jane','Gil-dong']
-for  f_name in names:
-    print(f'hello, {f_name}')
+    animals = ['dog','cat','hamster', 'tiger'] # in one line
+    foods = [
+        'Spagetti',
+        'Pizza',
+        'bibimbob'
+    ] # w/o trailing comma
+    names = [
+        'John',
+        'Jane',
+        'Gil-dong',
+        'Dong-eun',
+    ] # w/ trailing comma
+    for f_name in names:
+        print(f'hello, {f_name}')
 
 if __name__ == '__main__':
     print_hello()
